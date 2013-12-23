1f668d31e1ffcc649e195189756726a0e1f67627
Ghafran Abbas <ghafranabbas@gmail.com>
2013-12-23 17:02:22 -0500

    Initial Code

---
+++ test/helper.js
@@ -0,0 +1,25 @@
+// helper tests
+
+process.env.NODE_ENV = 'test';
+
+var expect = require('chai').expect;
+var helper = require('../lib/helper.js')();
+
+describe('helper', function() {
+	
+    describe('getDistanceFromLatLonInM', function() {
+		it('output > 2400 && output < 2500', function(){
+		      var output = helper.getDistanceFromLatLonInM(38.895095, -77.036361, 38.890098, -77.008667);
+			  output.should.above(2400);
+  			  output.should.below(2500);
+		    })
+    });
+
+    describe('getDistanceFromLatLonInKm', function() {
+		it('output > 2.4 && output < 2.5', function(done) {
+			var output = maps.getDistanceFromLatLonInKm(38.895095, -77.036361, 38.890098, -77.008667);
+			  output.should.above(2.4);
+			  output.should.below(2.5);
+		});
+	});
+});
